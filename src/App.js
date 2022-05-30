557b7a50045beb20e89619e9bf70342f6f022f0a
lima-anderson <msgli34@gmail.com>
2022-05-30 00:00:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Nav } from 'react-bootstrap';
+// import { Home } from './componentes/Home';
+// import { Sobre } from './componentes/Sobre';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Alunos } from './componentes/Alunos';
+import { BrowserRouter, Routes, Link, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Aplicação React</h1>
+
+      <BrowserRouter>
+        <Nav variant="tabs">
+          {/* <Nav.Link as ={Link} to='/'>Página Inicialive</Nav.Link> */}
+          <Nav.Link as ={Link} to='/alunos'>Cadastro de Alunos</Nav.Link>
+          {/* <Nav.Link as ={Link} to='/sobre'>Sobre</Nav.Link> */}
+        </Nav>
+
+      <Routes>
+        {/* <Route path='/' exact={true} element={<Home/>}></Route> */}
+        <Route path='/alunos' element={<Alunos/>}></Route>
+        {/* <Route path='/sobre' element={<Sobre/>}></Route> */}
+      </Routes>
+
+      </BrowserRouter>
     </div>
+    
   );
 }
 
